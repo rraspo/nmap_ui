77b325b8238de369bc29537d21cf9b6d3f8819a5
Juan A. Porras <juanabrahamporras@gmail.com>
2021-06-18 12:07:46 -0500

    Add eslint config. Add semantic ui dependency. Update app component with table. Commit yarn lock file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Container, Grid, Header, Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-function App() {
+function Dashboard() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ marginTop: '3em' }}>
+      <Header as='h1'>
+        NMAP dashboard
+      </Header>
+      <Header as='h2' dividing>
+        Scans
+      </Header>
+      <Grid columns={1}>
+        <Grid.Column>
+          <Table>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+                <Table.HeaderCell>Header</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              <Table.Row>
+                <Table.Cell>
+                  <Label ribbon>First</Label>Cell
+                </Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+              <Table.Row>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+                <Table.Cell>Cell</Table.Cell>
+              </Table.Row>
+            </Table.Body>
+            <Table.Footer>
+              <Table.Row>
+                <Table.HeaderCell colSpan='3'>
+                  <Menu floated='right' pagination>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron left' />
+                    </Menu.Item>
+                    <Menu.Item as='a'>1</Menu.Item>
+                    <Menu.Item as='a'>2</Menu.Item>
+                    <Menu.Item as='a'>3</Menu.Item>
+                    <Menu.Item as='a'>4</Menu.Item>
+                    <Menu.Item as='a' icon>
+                      <Icon name='chevron right' />
+                    </Menu.Item>
+                  </Menu>
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Footer>
+          </Table>
+        </Grid.Column>
+      </Grid>
+    </Container>
   );
 }
 
-export default App;
+export default Dashboard;
